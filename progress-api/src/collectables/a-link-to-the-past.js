7766deda2245a8f1a5cbce0fa3929b0fce692661
Tom Carter <heymrcarter14@gmail.com>
2017-02-03 18:29:52 -0500

    adds found property to bottles

--- progress-api/src/collectables/a-link-to-the-past.js
+++ progress-api/src/collectables/a-link-to-the-past.js
@@ -3,21 +3,26 @@ export default {
     {
       id: 'bottle-1',
       location: 'The first bottole is located in Kakariko Village in the Light World. It is in the northwest part of Hyrule. This bottle can be purchased for 100 Rupees. You buy it from the man that is sitting on top of a carpet.',
-      screenshots: []
+      screenshots: [],
+      found: false
     },
     {
       id: 'bottle-2',
       location: `Another Bottle is located in Kakariko Village in the Light World. It's in the tavern located in the southeastern part of Kakariko Village. It is the long building, so go into it and you will see a treasure on the other side, but you cannot reach it. Just go back outside and walk around to the other side of the long building. There is a secret entrance, so just walk into the building from the top of it and you will be on the other side of the tavern. Open the treasure chest to get the Bottle.`,
-      screenshots: []
+      screenshots: [],
+      found: false
     },
     {
       id: 'bottle-3',
       location: 'The third Bottle is a little hard to find. It is located just east of your house in the Light World. From your house go right a screen. The actual Bottle is located under the bridge. Go above the bridge and walk to your right. Keep walking until you find a place where you could jump into the river. Once you are in the river swim back towards the bridge and eventually under the bridge. You will see a man under the bridge. Talk to him and you will get the third Bottle.',
-      screenshots: []
+      screenshots: [],
+      found: false
     },
     {
       id: 'bottle-4',
-      location: `One last bottle, and it's the hardest to get of them all. You will need to get your sword tempered before you can get this Bottle. In the Dark World, go to the place where you got your sword tempered. It is just east of the Village of Outcasts. Where the house use to be will now be a run down place with a treasure chest there. Go next to the treasure and it will start to follow you.\n\nGo back into the main part of the Village of Outcasts with the treasure chest following you. You cannot jump down anything or the treasure chest will stay behind. Go south of the Village of Outcasts. Keep going down and then right. Make your way all the way to where your house would be if it were the Light World. From where your house would be, go south and then south, and keep going until you have reached the southern part of Hyrule. From where the second crystal is located head left. Keep going until you change screens. Then use the mirror and go into the Light World. From here go up a little and talk to the guy that is sitting next to the sign. Before he would just stand and say nothing, but talk to him with the treasure chest and he'll open it for you. The treasure inside of it is the last Bottle.`
+      location: `One last bottle, and it's the hardest to get of them all. You will need to get your sword tempered before you can get this Bottle. In the Dark World, go to the place where you got your sword tempered. It is just east of the Village of Outcasts. Where the house use to be will now be a run down place with a treasure chest there. Go next to the treasure and it will start to follow you.\n\nGo back into the main part of the Village of Outcasts with the treasure chest following you. You cannot jump down anything or the treasure chest will stay behind. Go south of the Village of Outcasts. Keep going down and then right. Make your way all the way to where your house would be if it were the Light World. From where your house would be, go south and then south, and keep going until you have reached the southern part of Hyrule. From where the second crystal is located head left. Keep going until you change screens. Then use the mirror and go into the Light World. From here go up a little and talk to the guy that is sitting next to the sign. Before he would just stand and say nothing, but talk to him with the treasure chest and he'll open it for you. The treasure inside of it is the last Bottle.`,
+      screenshots: [],
+      found: false
     }
   ],
   fairyFountains: [],
