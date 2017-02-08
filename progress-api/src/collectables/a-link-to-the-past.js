9b8a66a5d863b314f1af50f4a1071b81a5ad915a
Tom Carter <heymrcarter14@gmail.com>
2017-02-08 12:16:27 -0500

    adds all collectables for link to the past

--- progress-api/src/collectables/a-link-to-the-past.js
+++ progress-api/src/collectables/a-link-to-the-past.js
@@ -25,7 +25,298 @@ export default {
       found: false
     }
   ],
-  fairyFountains: [],
-  piecesOfHeart: [],
-  warps: []
+  fairyFountains: [
+    {
+      id: 'fairy-fountain-1',
+      location: `South of the Eastern Palace before you enter the area filled with ruins, there is a cave. Walk inside of it and you'll come across a Fairy Fountain at the end of it.`,
+      screenshots: [],
+      found: false
+    },
+    {
+      id: 'fairy-fountain-2',
+      location: `This one is also near the Eastern Palace. It is in a cave at the far east part of Hyrule. Simply walk into the cave and enter the Fairy Fountain.`,
+      screenshots: [],
+      found: false
+    },
+    {
+      id: 'fairy-fountain-3',
+      location: `In the swamp area, there is a cracked wall on the northeastern part of it. Use a Bomb to blow it up and enter the new cave to find a Fairy Fountain.`,
+      screenshots: [],
+      found: false
+    },
+    {
+      id: 'fairy-foundation-4',
+      location: `This one is around Link's house. Dash into the rocks piled on top of eachother with the Pegasus Boots to reveal a staircase. Walk down the stairs to find a Fairy Fountain.`,
+      screenshot: [],
+      found: false
+    },
+    {
+      id: 'fairy-foundation-5',
+      location: `Right before entering the desert area, go north instead to find a cave. Walk inside of it to find another Fairy Fountain.`,
+      screenshots: [],
+      found: false
+    },
+    {
+      id: 'fairy-foundation-6',
+      location: `Go to the small island in the middle of Lake Hylia and in the cave. You'll find the Pond of Happiness, but it harbors a small additional secret. Bomb the east wall to find a Fairy Fountain.`,
+      screenshots: [],
+      found: false
+    },
+    {
+      id: 'fairy-foundation-7',
+      location: `Walk on the shore of Lake Hylia. You'll find an icy cave of sorts. If you bomb the wall to the left of the cave entrance, you'll find the Ice Rod. Walk into the normal entrance to find a Fairy Fountain.`,
+      screenshots: [],
+      found: false
+    },
+    {
+      id: 'fairy-foundation-8',
+      location: `Walk east from the graveyard, and you'll come across a lone bush surrounded by grass. Cut down the bush and go down the hole. You'll find a Fairy Fountain.`,
+      screenshots: [],
+      found: false
+    },
+    {
+      id: 'fairy-foundation-9',
+      location: `After killing Agahnim for the first time, go to the eastern border of the Lost Woods. Dash into the odd looking tree with the Pegasus Boots and go down the hole. You'll find a Fairy Fountain when you land.`,
+      screenshots: [],
+      found: false
+    },
+    {
+      id: 'fairy-foundation-10',
+      location: `It's in the exact same place as the first one in the Light World, but in the Dark World instead.`,
+      screenshots: [],
+      found: false
+    },
+    {
+      id: 'fairy-foundation-11',
+      location: `In the same place as the other Fairy Fountain by Link's house in the Light World, but in the Dark World.`,
+      screenshots: [],
+      found: false
+    },
+    {
+      id: 'fairy-foundation-12',
+      location: `Go to where the cave with the Ice Rod was in the Light World in the Dark World. Enter the bombed cave and find the Fairy Fountain.`,
+      screenshots: [],
+      found: false
+    },
+    {
+      id: 'fairy-foundation-13',
+      location: `In the Swamp of Evil, there is a cave to the right of Misery Mire. Walk inside to find a Fairy Fountain.`,
+      screenshots: [],
+      found: false
+    },
+    {
+      id: 'fairy-foundation-14',
+      location: `East of Ganon's Tower, there is a boulder. Lift it up and go down the steps. Go north and use Ether to see the invisible path. Bomb the wall and go through. Bomb the other walls and go north to find a Fairy Fountain.`,
+      screenshots: [],
+      found: false
+    },
+    {
+      id: 'fairy-foundation-15',
+      location: `Also in the same maze, head left this time instead of north. Bomb the north wall north of the exit outside to find a Fairy Fountain.`,
+      screenshots: [],
+      found: false
+    }
+  ],
+  piecesOfHeart: [
+    {
+      id: 'heart-piece-1',
+      location: `Head to the south east entrance of the lost woods, north of the fortune teller, and go up until you see a tree stump cave. Directly above it is a patch of 3x3 grass. Slash the middle one, fall down the hole and grab heart piece number one!`,
+      screenshots: [],
+      found: false
+    },
+    {
+      id: 'heart-piece-2',
+      location: `Go to the northwestern part of Kakariko Village. You will see a cave, and a well right next to it. Run onto the cliff above it and jump down the well. Bomb the north wall after you land in the upper ledge in the cave. Throw the pots out of the way and open the chest to get a Heart Piece.`,
+      screenshots: [],
+      found: false
+    },
+    {
+      id: 'heart-piece-3',
+      location: `There is a green building of sorts in Kakariko Village. After you go in, walk down the stairs. Bomb the cracked wall on the north side of the room. Open the treasure chest to get the Heart Piece.`,
+      screenshots: [],
+      found: false
+    },
+    {
+      id: 'heart-piece-4',
+      location: `Go south of Kakariko Village and into the house up the steps. Bomb the left wall and walk outside and talk to the lady. She says you will get a reward if you get to the end of the track in 15 seconds or less. Doing this will earn you a Heart Piece.`,
+      screenshots: [],
+      found: false
+    },
+    {
+      id: 'heart-piece-5',
+      location: `There is a cave in the desert in southwestern Hyrule. Go inside of it and bomb the south wall. Open the chest to get a Heart Piece.`,
+      screenshots: [],
+      found: false
+    },
+    {
+      id: 'heart-piece-6',
+      location: `While going through the Desert Palace, you will come across a time where you will go outside. Simply go left, then straight down after you come out. You will find a Heart Piece at the end.`,
+      screenshots: [],
+      found: false
+    },
+    {
+      id: 'heart-piece-7',
+      location: `West of the Sanctuary are a few rocks. Pick up the big one and walk down the staircase. Open the chest to get a Heart Piece.`,
+      screenshots: [],
+      found: false
+    },
+    {
+      id: 'heart-piece-8',
+      location: `After buying the Zora Flippers for 500 Rupees, swim south down the waterfall. Go onto the land area near the middle of the area. Follow the path and grab the Heart Piece.`,
+      screenshots: [],
+      found: false
+    },
+    {
+      id: 'heart-piece-9',
+      location: `When you walk up the really big staircase on Death Mountain, jump down to one of the ledges into a cave. Walk around and follow the path until you reach the Heart Piece at the end.`,
+      screenshots: [],
+      found: false
+    },
+    {
+      id: 'heart-piece-10',
+      location: `At the foot of the Tower of Hera, there is a tile that goes into the Dark World. In the Dark World, step onto the spot west of the tile on where Spectacle Rock is in the Light World. You'll appear on the top of Spectacle Rock, and the Heart Piece is on the right side.`,
+      screenshots: [],
+      found: false
+    },
+    {
+      id: 'heart-piece-11',
+      location: `Keep walking on the right side of the Pyramid of Power in the Dark World until you find this Heart Piece at the dead-end.`,
+      screenshots: [],
+      found: false
+    },
+    {
+      id: 'heart-piece-12',
+      location: `Go to the Haunted Grove in the Dark World. It is the large forest area that the Flute Boy is in. Go south of it and use the Magic Mirror inside of the circle of bushes. Go inside of the cave in the Light World and get the Heart Piece behind the pots.`,
+      screenshots: [],
+      found: false
+    },
+    {
+      id: 'heart-piece-13',
+      location: `Travel to where Lake Hylia is in the Dark World. On the west side of it, there's a circle of shallow water. Use the Magic Mirror to teleport back to the Light World, and you'll end up on a small island with a Heart Piece in the Light World.`,
+      screenshots: [],
+      found: false
+    },
+    {
+      id: 'heart-piece-14',
+      location: `Go into the Swamp Ruins in southern Hyrule. Inside, you'll find a switch that releases the water. When you release it, walk back outside. You'll find the Heart Piece in the now drained water.`,
+      screenshots: [],
+      found: false
+    },
+    {
+      id: 'heart-piece-15',
+      location: `Find the graveyard in the Dark World. Walk to the northern part of it and walk up the steps. Use the Magic Mirror to teleport back to the Light World. Walk into the new cave and grab the Heart Piece.`,
+      screenshots: [],
+      found: false
+    },
+    {
+      id: 'heart-piece-16',
+      location: `After defeating Agahnim for the first time, go back to the place near the Lost Woods where the brothers were cutting a tree. Dash into it to make the leaves disappear, then go down the hole to get your prize.`,
+      screenshots: [],
+      found: false
+    },
+    {
+      id: 'heart-piece-17',
+      location: `Once you get the Magic Cape in the graveyard in the Light World, go to the entrance to Death Mountain in the Light World, but return to the same place in the Dark World. Enter the cave and use the Magic Cape to get across the rubber ball blocking the path. Exit the cave to get a Heart Piece.`,
+      screenshots: [],
+      found: false
+    },
+    {
+      id: 'heart-piece-18',
+      location: `In one of the houses in the Village of Outcasts, there is a game full of treasure chests. You can open two per game, so just keep playing until you find the Heart Piece in one of them.`,
+      screenshots: [],
+      found: false
+    },
+    {
+      id: 'heart-piece-19',
+      location: `Go east from the Village of Outcasts. Use the Titan's Mitt to lift up the big black rocks, then use the Magic Hammer on all of the stakes and a staircase in the ground will appear. Walk down the stairs to get the Heart Piece.`,
+      screenshots: [],
+      found: false
+    },
+    {
+      id: 'heart-piece-20',
+      location: `For 80 Rupees, you can play a game where you can dig up as many treasures as you want in 30 seconds with a Shovel. Keep playing it and eventually you'll unearth a Heart Piece.`,
+      screenshots: [],
+      found: false
+    },
+    {
+      id: 'heart-piece-21',
+      location: `Play the Flute and go location #6 on the map. Lift up the rock and step into the Dark World. There are three small caves coming out of the swamp. Inside one of them, you'll find a treasure chest with a Heart Piece.`,
+      screenshots: [],
+      found: false
+    },
+    {
+      id: 'heart-piece-22',
+      location: `Also in the Swamp of Evil, go to the northeast corner of it. Use the Magic Mirror to warp to a cliff in the desert in the Light World. Lift up the boulder and go down the stairs. Push yourself through the blocks to get the Heart Piece.`,
+      screenshots: [],
+      found: false
+    },
+    {
+      id: 'heart-piece-23',
+      location: `In the Dark World, go east from Ganon's Tower. Lift up the boulder and go down the steps. Walk north and you'll see a gap between you and a cracked wall. Or is it? Use the Ether Medallion to see the invisible path. Go through a small maze of bombing walls and you'll end up on a peak outside. Use the Magic Mirror and get the Heart Piece.`,
+      screenshots: [],
+      found: false
+    },
+    {
+      id: 'heart-piece-24',
+      location: `While you are going through Turtle Rock, you'll come across a part where you walk across a path outside. Use the Magic Mirror while you are in front of one of the caves and you'll be on a cliff with a cave in the Light World. Kill the enemies inside and open the chest to get a Heart Piece.`,
+      screenshots: [],
+      found: false
+    }
+  ],
+  warps: [
+    {
+      id: 'warp-1',
+      location: `After defeating Agahnim for the first time, the gate leading into Hyrule Castle becomes a portal into the Dark World. Simply walk through it to get into the Dark World.`,
+      screenshots: [],
+      found: false
+    },
+    {
+      id: 'warp-2',
+      location: `his'll be the first actual tile you'll encounter in the game. You have to go into it and use the Magic Mirror afterwards to get to the top of Death Mountain. It is located at the foot of the Tower of Hera.`,
+      screenshots: [],
+      found: false
+    },
+    {
+      id: 'warp-3',
+      location: `Go north from Kakariko Village. You can use the Titan's Mitt from Gargoyle's Domain, or simply go into the Lost Woods and hammer down the stakes from the other side.`,
+      screenshots: [],
+      found: false
+    },
+    {
+      id: 'warp-4',
+      location: `South of the area with the Eastern Palace is a tile. Use the Magic Hammer to push down the stakes and go into it.`,
+      screenshots: [],
+      found: false
+    },
+    {
+      id: 'warp-5',
+      location: `This one is in the northwestern corner of the swamp area in central southern Hyrule. Smash the stakes with the Magic Hammer and step onto it.`,
+      screenshots: [],
+      found: false
+    },
+    {
+      id: 'warp-6',
+      location: `There is a tile on the island at the middle of Lake Hylia. Stepping onto this one below the black rock will get you access into the Ice Palace. The Titan's Mitt is required.`,
+      screenshots: [],
+      found: false
+    },
+    {
+      id: 'warp-7',
+      location: `Play the Flute and warp to the desert. Lift up the black rock and step into the tile to get into the Swamp of Evil. Misery Mire is here, and there is no other way to get into this area.`,
+      screenshots: [],
+      found: false
+    },
+    {
+      id: 'warp-8',
+      location: `At the lower parts of Death Mountain, lift up the black rock with the Titan's Mitt from Gargoyle's Domain to find a tile below it.`,
+      screenshots: [],
+      found: false
+    },
+    {
+      id: 'warp-9',
+      location: `Go east from the Tower of Hera and lift up the big black rock. Smash down the stakes in the right order (listen for the sounds) to make the tile appear. This will grant you access to Turtle Rock.`,
+      screenshots: [],
+      found: false
+    }
+  ]
 };
